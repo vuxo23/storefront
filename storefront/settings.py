63b3f2d15718d8b53524809f744e0246fe6287b3
Vuxo Mashimby <vuxodawn@gmail.com>
2026-01-18 19:52:18 +0200

    Random data added to database in DataDrip using mysql

--- storefront/settings.py
+++ storefront/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'USER': 'vuxomashimby',
+        'PASSWORD': '@Mashimby040614',
     }
 }
 
