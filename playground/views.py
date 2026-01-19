75fda6748bc0bdf900aa7526baaf1d1f0b444b7b
Vuxo Mashimby <vuxodawn@gmail.com>
2026-01-20 01:46:17 +0200

    Created the superuser(Admin)

--- playground/views.py
+++ playground/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db import connection
+from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
+
 def say_hello(request):
-    x = 1
-    y = 2
-    query_set = Product.objects.all()
-    for product in query_set:
-        print(product)
-   
-    return render(request, 'playground/hello.html', {'name': 'Vuxo'})
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM store_product")
+        
+    return render(request, 'playground/hello.html', {'name': 'Vuxo', 'products': products})
 
 
 def firstapp(request):
