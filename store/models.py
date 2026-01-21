c9444421067696cf1d1b3583c243228f62dc66b3
Vuxo Mashimby <vuxodawn@gmail.com>
2026-01-22 00:42:35 +0200

    Added admin for app models

--- store/models.py
+++ store/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator
 class Promotion(models.Model):
     description = models.CharField(max_length=255)
     discount = models.FloatField()
@@ -7,19 +7,35 @@ class Promotion(models.Model):
 class Collection(models.Model):
     title= models.CharField(max_length=255)
     featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null= True, related_name='+' )
+    
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
+        
 
 
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(default="-")
-    description = models.TextField()
-    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
-    inventory = models.IntegerField()
+    description = models.TextField(null = True, blank=True)
+    unit_price = models.DecimalField(
+        max_digits=6, decimal_places=2,
+        validators=[MinValueValidator(1)]
+        )
+    inventory = models.IntegerField(validators=[MinValueValidator(0)])
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
-    # promotions = models.ManyToManyField(Promotion, related_name="products")
-    promotions = models.ManyToManyField(Promotion)
+
+    promotions = models.ManyToManyField(Promotion, blank = True)
+    
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
 
 class Customer(models.Model):
     MEMBERSHIP_Bronze = 'B'
@@ -37,7 +53,12 @@ class Customer(models.Model):
     phone = models.CharField(max_length= 255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default= MEMBERSHIP_Bronze)
-
+    
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+    
+    class Meta:
+        ordering = ['first_name' , 'last_name']
 
 class Order(models.Model):
     PAYMENT_Pending = 'P'
@@ -53,13 +74,15 @@ class Order(models.Model):
     payment = models.CharField(max_length=1,choices= PAYMENT_STATUS, default= PAYMENT_Pending )
     place_at = models.DateTimeField(auto_now_add=True)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    
+
 
 class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.PositiveSmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
-    product = models.ForeignKey(Product, on_delete=models.PROTECT)
-
+    
 
 class Cart(models.Model):
     created_at= models.DateTimeField(auto_now=True)
@@ -67,9 +90,7 @@ class Cart(models.Model):
 class CartItem(models.Model):
     quantity = models.PositiveSmallIntegerField()
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-
-     
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)  
 
 class Address(models.Model):
     street = models.CharField(max_length=255)
