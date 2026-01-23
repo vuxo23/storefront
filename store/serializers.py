5954a5045898566e65a036c031b9004e8e33e264
Vuxo Mashimby <vuxodawn@gmail.com>
2026-01-23 21:40:57 +0200

    Creating serials

---
+++ store/serializers.py
@@ -0,0 +1,19 @@
+from decimal import Decimal
+from store.models import Product, Collection
+from rest_framework import serializers
+
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
+    
+    
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Product
+        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+        
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    
+    def calculate_tax(self, product: Product):
+        return product.unit_price * Decimal(1.1)
--- storefront/settings.py
+++ storefront/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sessions',
+    'rest_framework',
     'playground',
     'store',
     'tags',
@@ -134,3 +135,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False
+}
