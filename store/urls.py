31d46ec9fe3f2071ec46e00334c80421387093a7
Vuxo Mashimby <vuxodawn@gmail.com>
2026-02-11 19:11:15 +0200

    Implement use of mixins

--- store/urls.py
+++ store/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('product/', views.product_list),
-    path('product/<int:id>/', views.product_detail),
+    path('product/', views.ProductList.as_view()),
+    path('product/<int:pk>/', views.ProductDetail.as_view()),
+    path('collection/', views.CollectionList.as_view()),
+    path('collection/<int:id>/', views.collection_detail)
 ]
