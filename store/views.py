60da4fa362c219f50996f941eb1c19f1ae68c1be
Vuxo Mashimby <vuxodawn@gmail.com>
2026-02-17 13:28:28 +0200

    Finished cart implementations

--- store/views.py
+++ store/views.py
@@ -1,6 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
-from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
+from rest_framework.mixins import DestroyModelMixin, CreateModelMixin, RetrieveModelMixin
 from rest_framework.viewsets import ModelViewSet,GenericViewSet
 from rest_framework.response import Response
 from rest_framework.filters import SearchFilter, OrderingFilter
@@ -63,17 +63,25 @@ class ReviewViewSet(ModelViewSet):
     def get_serializer_context(self):
         return {'product_id': self.kwargs['product_pk']}
     
-class CartViewSet(CreateModelMixin, GenericViewSet, RetrieveModelMixin):
+class CartViewSet(CreateModelMixin, 
+                GenericViewSet,
+                RetrieveModelMixin,
+                DestroyModelMixin):
+    
     queryset = Cart.objects.prefetch_related('items__product').all()
     serializer_class = CartSerializer
-    
-    
-class CartItemViewSet(ModelViewSet):
-    
-    serializer_class = CartItemSerializer
-    def get_queryset(self):
-        return CartItem.objects.filter(cart_id= self.kwargs['cart_pk'])
-    
+class cartItemViewSet(ModelViewSet):
+    http_method_names = ['get', 'post', 'patch', 'delete']
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return AddCartItemSerializer
+        if self.request.method == 'PATCH':
+            return UpdateCartItemSerializer
+        return CartItemSerializer
     def get_serializer_context(self):
         return {'cart_id': self.kwargs['cart_pk']}
+        
+    def get_queryset(self):
+        return CartItem.objects.filter(cart_id= self.kwargs['cart_pk']).select_related('product')
     
