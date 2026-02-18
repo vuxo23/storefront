f415be29552ef2678adf825db4492ed164911bee
Vuxo Mashimby <vuxodawn@gmail.com>
2026-02-18 11:52:12 +0200

    Simple jwt tokens

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.contenttypes.admin import GenericTabularInline
 from store.admin import ProductAdmin
 from tags.models import TaggedItem
 from store.models import Product
+from .models import User
 
-
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username",  'first_name' , 'last_name', 'email', "usable_password", "password1", "password2"),
+            },
+        ),
+    )
 
 class TagInline(GenericTabularInline):
     autocomplete_fields = ['tag']
--- store/admin.py
+++ store/admin.py
@@ -54,7 +54,9 @@ class CustomerAdmin(admin.ModelAdmin):
     list_display = [ 'first_name' , 'last_name', 'membership' , 'order_count']
     list_editable = ['membership']
     list_per_page = 10
+    list_select_related = ['user']
     search_fields = ['first_name__istartswith', 'last_name__istartswith']
+    
     
     @admin.display(ordering='order_count')
     def order_count(self, customer):
