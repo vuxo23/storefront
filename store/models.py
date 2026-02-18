574777801aa7a674d28de165f90bb420a6f54e15
Vuxo Mashimby <vuxodawn@gmail.com>
2026-02-18 16:39:06 +0200

    Order api

--- store/models.py
+++ store/models.py
@@ -90,6 +90,7 @@ class Order(models.Model):
     
     class Meta:
         permissions = [
+
             ('cancel_order', 'can cancel order')
         ]
     
@@ -99,7 +100,7 @@ class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='OrderItem')
     quantity = models.PositiveSmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
-    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name='items')
     
     
     
--- store/serializers.py
+++ store/serializers.py
@@ -1,6 +1,8 @@
 from decimal import Decimal
-from store.models import Product, Collection, Promotion, Customer, Review, Cart, CartItem
+from django.db import transaction
 from rest_framework import serializers
+from .models import Product, Collection, Promotion, Customer, Review, Cart, CartItem, Order, OrderItem
+
 
 
 
@@ -9,12 +11,6 @@ class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Collection
         fields = ['id', 'title', 'products_count']
-        
-    
-        
-        
-
-    
     
 class ProductSerializer(serializers.ModelSerializer):
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
@@ -108,4 +104,52 @@ class UpdateCartItemSerializer(serializers.ModelSerializer):
         model = CartItem
         fields = ['quantity']
         
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product = simpleProductSerializer()
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'product', 'unit_price', 'quantity']
+        
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many = True)
+    class Meta:
+        model = Order
+        fields = ['id', 'customer', 'place_at',  'payment', 'items']
+        
+class UpdateOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['payment']
+class CreateOrderSerializer(serializers.Serializer):
+    cart_id = serializers.UUIDField()
     
+    def validate_cart_id(self,cart_id):
+        if not Cart.objects.filter(pk=cart_id).exists():
+            raise serializers.ValidationError('No cart with given id found')
+        
+        if CartItem.objects.filter(art_id =cart_id).count() == 0:
+            raise serializers.ValidationError('The cart is empty')
+        return cart_id
+    
+    def save(self, **kwargs):
+        
+        with transaction.atomic():
+            cart_id =self.validated_data['cart_id']
+            (customer, created) = Customer.objects.get_or_create(user_id = self.context['user_id'])
+            order = Order.objects.create(customer = customer)
+            
+            cart_Items = CartItem.objects.select_related('product').filter(cart_id=cart_id)
+            order_items = [
+                OrderItem(
+                order = order,
+                product = item.product,
+                unit_price = item.product.unit_price,
+                quantity = item.quantity
+                ) for item in cart_Items
+                ]
+            OrderItem.objects.bulk_create(order_items)
+            
+            Cart.objects.filter(pk= cart_id).delete()
+            
+            return order
