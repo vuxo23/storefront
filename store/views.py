b2b95aca0639b287128c3e6d45454f302dd39e45
Vuxo Mashimby <141929548+vuxo23@users.noreply.github.com>
2026-05-08 13:08:12 +0200

    performance testing

--- store/views.py
+++ store/views.py
@@ -16,6 +16,7 @@ from django.db.models import Count
 
 
 class ProductViewSet(ModelViewSet):
+    #queryset = Product.objects.all()
     queryset = Product.objects.prefetch_related('images').all()
     serializer_class  = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
